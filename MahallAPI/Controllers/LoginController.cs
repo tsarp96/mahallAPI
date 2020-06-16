fe656f0c110ca997650625641b6162d600937316
tsarp96 <tsarp96@hotmail.com>
2020-06-17 00:11:56 +0300

    empty LoginController

---
+++ MahallAPI/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MahallAPI.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class Login : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Mertkan geliyorum, bekle!";
+        }
+    }
+}
